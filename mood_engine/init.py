10ecee1f7d5e541e1bf4e556b6e772ace397b620
Navaneeth Nair <navaneethnair82@gmail.com>
2025-11-07 21:06:26 +0530

    added a mood engine

---
+++ mood_engine/init.py
@@ -0,0 +1,7 @@
+
+from eloengine import MoodElo, MoodState
+
+__all__ = ["MoodElo", "MoodState"]
+
+__version__ = "1.0.0"
+__author__ = "Navaneeth Nair","Rakesh Telang" ,"Prajakta Patil", "Jayeed Tamboli","Abhijit Patil"
